76a493506aa28b0b6e2443131de19405b7390bde
baselreal <basriibrahim67067@gmail.com>
2022-12-05 19:49:26 +0700

    maintenance

--- src/App.js
+++ src/App.js
@@ -5,43 +5,26 @@ import Home1 from './screens/homepage1/home1/home1';
 // import Info from './screens/homepage1/info/info';
 import Homecon from './screens/homepage1/homecontent/homecon';
 import Favorite from './screens/homepage1/favorite/favorite';
-<<<<<<< HEAD
 import Chizu from './screens/homepage1/map/chizu';
 import Partner from './screens/homepage1/partner/partner';
 import Testimoni from './screens/homepage1/testimoni/testimoni';
 import Slider from './components/testicom/assets/utility/slider';
 import Promo from './screens/homepage1/promo/promo';
 import Footer from './screens/homepage1/footer/footer';
-=======
-import Chizu from './screens/homepage1/map/chizu';  
-import Partner from './screens/homepage1/partner/partner';
-import Testimoni from './screens/homepage1/testimoni/testimoni';
->>>>>>> 45c9720ae58412f65192435b666b0915bff778b4
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <div>
         <Navbar />
         <Home1/>
         {/* <Info/> */}
       </div>
-=======
-
-      <Navbar/>
-      <div className='flex flex-col'>
-      <Home1/>
-      <Info/>
-      </div>
-
->>>>>>> 45c9720ae58412f65192435b666b0915bff778b4
       <Homecon/> 
       <Favorite/>
       <Chizu />
       <Partner/>
-<<<<<<< HEAD
       <div>
         <Testimoni/>
         <Slider />
@@ -50,10 +33,6 @@ function App() {
       <Promo />
       <Footer />
       </div>
-=======
-      <Testimoni/>
-
->>>>>>> 45c9720ae58412f65192435b666b0915bff778b4
      </div>
   );
 }
